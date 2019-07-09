7517107b36938b0d2fd99be3bc9fc10823941c64
bingoohuang <bingoo.huang@gmail.com>
2019-07-09 14:10:01 +0800

    demo download

--- src/main/java/com/github/bingoohuang/springboottrial/controller/DlController.java
+++ src/main/java/com/github/bingoohuang/springboottrial/controller/DlController.java
@@ -1,6 +1,7 @@
 package com.github.bingoohuang.springboottrial.controller;
 
 import com.github.bingoohuang.springboottrial.util.MimeTypes;
+import com.google.common.base.Charsets;
 import lombok.SneakyThrows;
 import lombok.val;
 import org.springframework.core.io.ClassPathResource;
@@ -13,24 +14,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URLEncoder;
 import java.nio.file.Files;
-import java.security.SecureRandom;
 
 
 @RestController
 public class DlController {
-    @GetMapping("/dl") @SneakyThrows
+    @GetMapping("/dl")
+    @SneakyThrows
     public ResponseEntity<byte[]> downloadByDocumentId(@Param("filename") String filename, @Param("utf8") boolean utf8) {
-        HttpHeaders headers = new HttpHeaders();
-        byte[] bytes = new byte[20];
         val cpr = new ClassPathResource(filename);
-        if (cpr.exists()) {
-            bytes = Files.readAllBytes(cpr.getFile().toPath());
+        byte[] bytes = cpr.exists() ? Files.readAllBytes(cpr.getFile().toPath()) : "我是黄进兵".getBytes(Charsets.UTF_8);
+
+        val headers = new HttpHeaders();
+        String f = filename;
+        if (utf8) {
+            headers.add("Content-Type", MimeTypes.getFileMimeType(filename) + "; charset=UTF-8");
+            f = URLEncoder.encode(filename, "UTF-8");
         } else {
-            SecureRandom.getInstanceStrong().nextBytes(bytes);
+            headers.add("Content-Type", MimeTypes.getFileMimeType(filename));
         }
 
-        headers.add("Content-Type", MimeTypes.getFileMimeType(filename) + (utf8 ? ";charset=UTF-8" : ""));
-        val f = utf8 ? URLEncoder.encode(filename, "UTF-8") : filename;
         headers.add("Content-Disposition", "attachment;filename=" + f + ";filename*=UTF-8''" + f);
         return new ResponseEntity<>(bytes, headers, HttpStatus.OK);
     }
